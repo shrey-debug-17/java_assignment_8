3ad2d4fdb292a2b0b5dc011276433cb591919cbc
shrey-debug-17 <ardeshanashrey17@gmail.com>
2025-04-19 14:17:36 +0530

    add InvalidPositionException for invalid index access

---
+++ InvalidPositionException.java
@@ -0,0 +1,9 @@
+//InvalidPositionException.java
+
+package com.studentdata.exceptions;
+
+public class InvalidPositionException extends Exception {
+    public InvalidPositionException(String message) {
+        super(message);
+    }
+}
