cb72a2afc84e0cec91ee6c4d062b91199a342dd4
shrey-debug-17 <ardeshanashrey17@gmail.com>
2025-04-19 14:16:59 +0530

    add StudentNotFoundException for missing student records

---
+++ StudentNotFoundException.java
@@ -0,0 +1,9 @@
+//StudentNotFoundException.java
+
+package com.studentdata.exceptions;
+
+public class StudentNotFoundException extends Exception {
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
