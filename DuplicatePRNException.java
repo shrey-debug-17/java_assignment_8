34df6d627db0de9b9b05c225b04821eef87616f5
shrey-debug-17 <ardeshanashrey17@gmail.com>
2025-04-19 14:14:39 +0530

    add DuplicatePRNException for duplicate PRN validation

---
+++ DuplicatePRNException.java
@@ -0,0 +1,9 @@
+//DuplicatePRNException.java
+
+package com.studentdata.exceptions;
+
+public class DuplicatePRNException extends Exception {
+    public DuplicatePRNException(String message) {
+        super(message);
+    }
+}
