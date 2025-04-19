4758dcf43c0aa57e9d1f0aadd6a051a0e2995975
shrey-debug-17 <ardeshanashrey17@gmail.com>
2025-04-19 14:15:05 +0530

    add InvalidDOBException for DOB format validation

---
+++ InvalidDOBException.java
@@ -0,0 +1,9 @@
+//InvalidDOBException.java
+
+package com.studentdata.exceptions;
+
+public class InvalidDOBException extends Exception {
+    public InvalidDOBException(String message) {
+        super(message);
+    }
+}
